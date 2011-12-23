6214ef67c3db8f309e6da157a8638d9dda4995a4
Rahoul Baruah <rahoul@3hv.co.uk>
2011-12-23 15:58:42 +0000

    udpated job spec

--- spec/mercury/job_spec.rb
+++ spec/mercury/job_spec.rb
@@ -3,11 +3,14 @@ require 'lib/mercury'
 describe Mercury::Job do
   describe "when given a valid category" do
     it "should send a POST to each URL within the configuration file" do
-      Mercury::Configure.when_receiving "hello", send_to: ["http://www.example.com", "http://www.whatever.com/wherever"]
+      Mercury::Configuration.forward 'my_message', to: ['http://example.com']
+
+      # fakeweb here for testing the post to example.com
+      Mercury::Job.perform 'my_message', { :field => 'value', :this => 'that' }
     end
   end
 
   describe "when given an invalid category" do
-    it "should do nothing"
+    it "should log an error message"
   end
 end
